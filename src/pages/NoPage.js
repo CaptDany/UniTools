d9bcbfb86962ab0cd5c56196f4a4262e96bc726b
Capt. Dany <st3796@utr.edu.mx>
2023-07-17 02:35:34 -0600

    Deployed website

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -9,7 +9,7 @@ function NoPage() {
     <TopBar subpage={false}/>
     <h1 className="home">Error 404</h1>
     <h2 className="home">Seems that you're lost, why don't you try to find yourself?</h2>
-    <Link href="https://captdany.github.io/UniTools/home" className="home">Back home</Link>
+    <Link to="/UniTools/home" className="home">Back home</Link>
     <div style={{height:"40vh"}}/>
     <div style={{display: "flex",justifyContent: "center", alignContent: "center", alignItems: "center"}}>
       <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3EzYTl1M3hxNTZ1YnBzZzVqZWMzZXliNjFzM2RzcXh4eGhrbGpxbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26FxrH3l4kLYR7O3C/giphy.gif" alt="lost cat"/>
